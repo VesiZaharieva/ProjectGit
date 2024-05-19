9b10047c1f95b089890403e1f258f19aa6c9148d
VesiZaharieva <vesela.zaharieva@abv.bg>
2024-05-19 19:04:12 +0300

    .

--- src/test/java/selenium/pages/RegistrationPage.java
+++ src/test/java/selenium/pages/RegistrationPage.java
@@ -3,7 +3,10 @@ package selenium.pages;
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 import selenium.base.BasePage;
+
+import static org.testng.Assert.assertEquals;
 
 public class RegistrationPage extends BasePage {
 
@@ -13,8 +16,10 @@ public class RegistrationPage extends BasePage {
     @FindBy(id = "email")
     private WebElement emailField;
 
-    @FindBy(id = "email-error")
+    @FindBy(css = ".mb-0")
     private WebElement emailErrorMessage;
+
+
 
     @FindBy(css = "[data-continue-to=password-container]")
     private WebElement continueButton;
@@ -31,13 +36,27 @@ public class RegistrationPage extends BasePage {
         emailField.sendKeys(email);
     }
 
-    @Step("Validate the entered e-mail")
-    public Boolean validateEmail() {
+    @Step("E-mail error message displaying")
+    public Boolean notValidEmailMessage() {
         return emailErrorMessage.isDisplayed();
     }
+    @Step("Get the e-mail error message text")
+    public String notValidEmailMessageText(){
+        return emailErrorMessage.getText();
+    }
 
+
+    @Step("Check the Continue button colour")
+    public String getContinueButtonColor(){
+        return continueButton.getCssValue("color");
+    }
     @Step("Continue after e-mail entering")
     public void clickContinueButton() {
+        waitForElementTobeClickable(continueButton);
         continueButton.click();
+    }
+    @Step("Check the continue button invisibility")
+    public Boolean continueButtonInvisible(){
+        return continueButton.isDisplayed();
     }
 }
--- src/test/java/selenium/tests/RegistrationPageTest.java
+++ src/test/java/selenium/tests/RegistrationPageTest.java
@@ -11,6 +11,7 @@ import utils.CsvReader;
 
 import java.io.IOException;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
 
@@ -22,19 +23,68 @@ public class RegistrationPageTest extends MainTest {
     public static Object[][] dataProviderCsvEnterValidEmail() throws IOException, CsvException {
         return CsvReader.readFile("src/test/resources/valid-email.csv");
     }
+    @DataProvider(name = "notvalid-email")
+    public static Object[][] dataProviderCsvEnterNotValidEmail() throws IOException, CsvException {
+        return CsvReader.readFile("src/test/resources/notvalid-email.csv");
+    }
 
     @Epic("User registration")
     @Feature("E-mail registration")
-    @Story("Enter correct e-mail")
+    @Story("Enter not valid e-mail")
     @Severity(SeverityLevel.NORMAL)
-    @Test(dataProvider = "valid-email")
-    public void enterEmail(String email) {
+    @Test(dataProvider = "notvalid-email")
+    public void NotValidEmailMessage(String email) {
         homepage = new HomePage();
         homepage.clickSignUpButton();
         registrationPage = new RegistrationPage();
         registrationPage.emailContainerDisplayed();
         registrationPage.enterEmail(email);
-        assertFalse(registrationPage.validateEmail());
+        registrationPage.clickContinueButton();
+        assertTrue(registrationPage.notValidEmailMessage());
+    }
+    @Epic("User registration")
+    @Feature("E-mail registration")
+    @Story("Enter not valid e-mail")
+    @Severity(SeverityLevel.NORMAL)
+    @Test(dataProvider = "notvalid-email")
+    public void NotValidEmailMessageText(String email) {
+        homepage = new HomePage();
+        homepage.clickSignUpButton();
+        registrationPage = new RegistrationPage();
+        registrationPage.emailContainerDisplayed();
+        registrationPage.enterEmail(email);
+        registrationPage.clickContinueButton();
+        assertEquals(registrationPage.notValidEmailMessageText(), "Email is invalid or already taken");
+    }
+
+    @Epic("User registration")
+    @Feature("E-mail registration")
+    @Story("Enter valid e-mail")
+    @Severity(SeverityLevel.NORMAL)
+    @Test(dataProvider = "valid-email")
+    public void enterValidEmail(String email) {
+        homepage = new HomePage();
+        homepage.clickSignUpButton();
+        registrationPage = new RegistrationPage();
+        registrationPage.emailContainerDisplayed();
+        registrationPage.enterEmail(email);
+        String color = registrationPage.getContinueButtonColor();
+        assertEquals(color, "rgb(98, 117, 151)");
+    }
+    @Epic("User registration")
+    @Feature("E-mail registration")
+    @Story("Enter valid e-mail")
+    @Severity(SeverityLevel.NORMAL)
+    @Test(dataProvider = "valid-email")
+    public void continueWithValidEmail(String email) {
+        homepage = new HomePage();
+        homepage.clickSignUpButton();
+        registrationPage = new RegistrationPage();
+        registrationPage.emailContainerDisplayed();
+        registrationPage.enterEmail(email);
+        registrationPage.clickContinueButton();
+        assertFalse(registrationPage.continueButtonInvisible());
 
     }
+
 }
